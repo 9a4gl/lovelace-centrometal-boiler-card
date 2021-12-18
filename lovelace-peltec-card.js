afa5443228a3d277ec0cfe4bcd1dc31bf4257a04
Tihomir Heidelberg <tihomir.heidelberg@lite.hr>
2021-12-18 22:33:49 +0000

    some work

--- lovelace-peltec-card.js
+++ lovelace-peltec-card.js
@@ -18,42 +18,67 @@ class LoveacePelTecCard extends LitElement {
 
   shouldUpdate(changedProperties) {
     if (changedProperties.has("config")) {
-      console.log("config changed");
+      // console.log("config changed");
       return true;
     }
     if (changedProperties.has("hass")) {
-      console.log("hass changed");
+      // console.log("hass changed");
       const oldHass = changedProperties.get("hass");
-      this.config.entities.forEach((entityName) => {
-        const oldValue = oldHass.states[entityName];
-        const newValue = this.hass.states[entityName];
+      this.parameters.forEach((parameter) => {
+        const oldValue = oldHass.states[parameter];
+        const newValue = this.hass.states[parameter];
         if (oldValue != newValue) {
-          console.log("%s : %s != %s", entityName, oldValue.state, newValue.state);
+          console.log("%s : %s != %s", parameter, oldValue.state, newValue.state);
+          return true;
         }
       });
-      return true;
-      // return changedProperties.has('peltec');
+      this.optional_parameters.forEach((parameter) => {
+        const oldValue = oldHass.states[parameter];
+        const newValue = this.hass.states[parameter];
+        if (oldValue != newValue) {
+          console.log("%s : %s != %s", parameter, oldValue.state, newValue.state);
+          return true;
+        }
+      });
+      return false;
     }
-    console.log("unknown changed");
+    // console.log("unknown changed");
     return false;
   }
 
   render() {
-    const str = this.display.createContent();
+    const str = this.display.createContent(this.hass, this.config);
     var div = document.createElement('div');
     div.innerHTML = str.trim();
+    div = div.firstChild;
     return html`
-      <ha-card>
-        <div class="card-content" style="padding: 0px;">${div}</div>
-      </ha-card>`;
+      <ha-card>${div}</ha-card>`;
+  }
+
+  checkParameters(parameters) {
+    var missing = [];
+    this.parameters.forEach((parameter) => {
+      if (!(parameter in this.config)) {
+        missing.push(parameter);
+      }
+    });
+    return missing;
   }
 
   setConfig(config) {
-    if (!config.entities) {
-      throw new Error("You need to define entities");
-    }
     this.config = config;
-    this.style.cssText = "display: block; width: 100%;";
+    this.parameters = ["peltec_state", "peltec_fire_sensor", "peltec_fan",
+      "peltec_boiler_pump", "peltec_boiler_pump_demand", "peltec_electric_heater",
+      "peltec_buffer_tank_temparature_up", "peltec_buffer_tank_temparature_down",
+      "peltec_lambda_sensor", "peltec_tank_level", "peltec_configuration",
+      "peltec_boiler_temperature", "peltec_mixer_temperature", "peltec_mixing_valve",
+      "peltec_flue_gas", "peltec_active_command"];
+    this.optional_parameters = ["peltec_outdoor_temperature"]
+    const missing = this.checkParameters();
+    if (missing.length > 0) {
+      throw new Error("You need to define: " + missing.join(","));
+    }
+    this.style.cssText = "display: block;";
   }
 
   getCardSize() {
--- peltec-display.js
+++ peltec-display.js
@@ -72,74 +72,117 @@ export default class PelTecDisplay {
         return str;
     }
 
-    createContent()
+    createContent(hass, config)
     {
         var lineHeight = (20 * this.factor).toString();
-        var str = "<div style=\"position: relative; top: 0; left: 0; width: auto; height: auto; line-height: " + lineHeight + "px;\">\n";
+        var str = "<div class=\"card-content\" style=\"position: relative; top: 0; left: 0; padding: 0px; width: auto; height: auto; line-height: " + lineHeight + "px;\">\n";
 
         str += "<img src=\"/local/lovelace-peltec-card/images/background.png\" style=\"width: 100%; top: 0; left: 0; position: relative;\" />\n";
 
-        // cc.params['B_FotV'].v < 1000
-        str += this.createImage("vatra.gif", 160, 305, 80, null);
+        const peltec_state = hass.states[config.peltec_state].state ?? 0;
+        const active_command = hass.states[config.peltec_active_command].state ?? 0;
+        const fire_sensor_value = hass.states[config.peltec_fire_sensor].state ?? 0;
+        const fan_value = hass.states[config.peltec_fan].state ?? 0;
+        const boiler_pump_value = hass.states[config.peltec_boiler_pump].state ?? 0;
+        const boiler_pump_demand_value = hass.states[config.peltec_boiler_pump_demand].state ?? 0;
+        const electric_heater_value = hass.states[config.peltec_electric_heater].state ?? 0;
+        const boiler_temperature_value = hass.states[config.peltec_boiler_temperature].state ?? 0;
+        const flue_gas_value = hass.states[config.peltec_flue_gas].state ?? 0;
+        const mixer_temperature_value = hass.states[config.peltec_mixer_temperature].state ?? 0;
+        const lambda_sensor_value = hass.states[config.peltec_lambda_sensor].state ?? 0;
+        const mixing_valve_value = hass.states[config.peltec_mixing_valve].state ?? 0;
+        const outdoor_temperature = ("peltec_outdoor_temperature" in hass.states) ? (hass.states[config.peltec_outdoor_temperature].state ?? 0) : false;
+        const tank_level_value = hass.states[config.peltec_tank_level].state ?? 0; // "Full"
+        const configuration_value = hass.states[config.peltec_configuration].state ?? 0; // "4. BUF"
+        const buffer_tank_temparature_up_value = hass.states[config.peltec_buffer_tank_temparature_up].state ?? 0;
+        const buffer_tank_temparature_down_value = hass.states[config.peltec_buffer_tank_temparature_down].state ?? 0;
 
-        // cc.params['B_FotV'].v >= 0
-        str += this.createText("&gt;1M", 20, "color: #000000;", 120, 360);
+        if (fire_sensor_value < 1000) {
+            // cc.params['B_FotV'].v < 1000
+            str += this.createImage("vatra.gif", 160, 305, 80, null);
+        }
 
-        // cc.params['B_fan'].v == 0
-        str += this.createImage("ventilatorStoji-unit.png", 35, 210, 100, null);
+        if (fire_sensor_value > 0) {
+            // cc.params['B_FotV'].v >= 0
+            str += this.createText("&gt;1M", 20, "color: #000000;", 120, 360);
+        }
 
-        // cc.params['B_fan'].v != 0
-        str += this.createImage("ventilator-unit.gif", 35, 210, 100, null);
+        if (fan_value == 0) {
+            // cc.params['B_fan'].v == 0
+            str += this.createImage("ventilatorStoji-unit.png", 35, 210, 100, null);
+        }
+
+        if (fan_value != 0) {
+            // cc.params['B_fan'].v != 0
+            str += this.createImage("ventilator-unit.gif", 35, 210, 100, null);
+        }
 
         // Ventilator speed?
-        str += this.createText("MAX", 20, "color: #000000;", 140, 255);
+        str += this.createText(fan_value, 20, "color: #000000;", 140, 255);
 
-        // cc.params['B_P1'].v == 0
-        str += this.createImage("pumpaStojiLijevo.png", 345, 212, 66, null);
+        if (boiler_pump_value == 0) {
+            // cc.params['B_P1'].v == 0
+            str += this.createImage("pumpaStojiLijevo.png", 345, 212, 66, null);
+        }
 
-        // cc.params['B_P1'].v &amp;&amp; (cc.params['B_P1'].v == 1)
-        str += this.createImage("pumpaokrece.gif", 345, 212, 66, null);
+        if (boiler_pump_value == 1) {
+            // cc.params['B_P1'].v &amp;&amp; (cc.params['B_P1'].v == 1)
+            str += this.createImage("pumpaokrece.gif", 345, 212, 66, null);
+        }
 
-        // cc.params['B_zahP1'].v == 1
-        str += this.createImage("demand_p.png", 345, 240, 12, null);
+        if (boiler_pump_demand_value == 1) {
+            // cc.params['B_zahP1'].v == 1
+            str += this.createImage("demand_p.png", 345, 240, 12, null);
+        }
 
-        // cc.params['B_gri'].v == 0
-        str += this.createImage("heater.png", 70, 435, 56, null);
+        if (electric_heater_value == 0) {
+            // cc.params['B_gri'].v == 0
+            str += this.createImage("heater.png", 70, 435, 56, null);
+        }
 
-        // cc.params['B_gri'].v &amp;&amp; (cc.params['B_gri'].v == 1)
-        str += this.createImage("grijac.png", 70, 435, 56, null);
+        if (electric_heater_value == 1) {
+            // cc.params['B_gri'].v &amp;&amp; (cc.params['B_gri'].v == 1)
+            str += this.createImage("grijac.png", 70, 435, 56, null);
+        }
 
-        str += this.createText("66.2 °C", 42, "color: #FFFFFF;",  100, 157);
+        str += this.createText(boiler_temperature_value + " °C", 42, "color: #FFFFFF;",  100, 157);
 
         str += this.createImage("senzor_b_1.png", 90, 40, 50, null);
 
-        str += this.createText("106 °C", 26, "color: #FFFFFF;",  155, 44);
+        str += this.createText(flue_gas_value + " °C", 26, "color: #FFFFFF;",  155, 44);
 
         str += this.createImage("senzor_d.png", 425, 240, 30, null);
 
-        str += this.createText("63 °C", 26, "color: #FFFFFF;", 460, 262, 80);
+        str += this.createText(mixer_temperature_value + " °C", 26, "color: #FFFFFF;", 460, 262, 80);
 
-        // (cc.params['B_Oxy1'].v &amp;&amp; (cc.params['B_Oxy1'].v > 0.1 ))
-        str += this.createImage("senzor_b_1.png", 90, 90, 50, null);
+        if (lambda_sensor_value > 0.1) {
+            // (cc.params['B_Oxy1'].v &amp;&amp; (cc.params['B_Oxy1'].v > 0.1 ))
+            str += this.createImage("senzor_b_1.png", 90, 90, 50, null);
+            // cc.params['B_Oxy1'].v &amp;&amp; (cc.params['B_Oxy1'].v >= 0.1)
+            str += this.createText("-% O<sub>2</sub>", 20, "color: #e0e3ff;", 155, 90);
+        }
 
-        // cc.params['B_Oxy1'].v &amp;&amp; (cc.params['B_Oxy1'].v >= 0.1)
-        str += this.createText("-% O<sub>2</sub>", 20, "color: #e0e3ff;", 155, 90);
+        str += this.createText(mixing_valve_value + "%", 22, "color: #ffffff;", 340, 139, 80);
 
-        str += this.createText("100%", 22, "color: #ffffff;", 340, 139, 80);
+        if (outdoor_temperature !== false) {
+            str += this.createImage("vanjska.png", 600, 20, 20, "auto");
+            str += this.createText(outdoor_temperature + "°C", 28, "color: #ffffff;", 530, 30);
+        }
 
-        str += this.createImage("vanjska.png", 600, 20, 20, "auto");
+        if (tank_level_value == "Full") {
+            // cc.params[ 'B_razina' ].v &amp;&amp; (cc.params['B_razina'].v == 2)
+            str += this.createImage("spremnikpun-n.png", 469, 330, 19, "auto");
+        }
+        if (tank_level_value == "Reserve") {
+            // cc.params[ 'B_razina' ].v &amp;&amp; (cc.params['B_razina'].v == 1)
+            str += this.createImage("rezerva-n.png", 469, 330, 19, "auto");
+        }
+        if (tank_level_value == "Empty") {
+            // cc.params[ 'B_razina' ].v &amp;&amp; (cc.params['B_razina'].v == 0)
+            str += this.createImage("nemapeleta-n.png", 469, 330, 19, "auto");
+        }
 
-        str += this.createText("--°C", 28, "color: #ffffff;", 530, 30);
-
-        // cc.params[ 'B_razina' ].v &amp;&amp; (cc.params['B_razina'].v == 2)
-        str += this.createImage("spremnikpun-n.png", 469, 330, 19, "auto");
-
-        // cc.params[ 'B_razina' ].v &amp;&amp; (cc.params['B_razina'].v == 1)
-        str += this.createImage("rezerva-n.png", 469, 330, 19, "auto");
-
-        // cc.params[ 'B_razina' ].v &amp;&amp; (cc.params['B_razina'].v == 0)
-        str += this.createImage("nemapeleta-n.png", 469, 330, 19, "auto");
-
+        /*
         // cc.params['B_addConf'].v | hexBitIsSet: 3
         str += "<div " + this.createStyle("position:absolute; z-index: 2;", 375, 300, 80, 30) + "\">";
         str += this.createImage("fuel_percentage.png", 0, 0,
@@ -157,17 +200,21 @@ export default class PelTecDisplay {
         str += "<div " + this.createStyle("position:absolute; z-index: 2;", 800, 10, 80, 90) + "\">";
         str += this.createImage("radijatorSlavina.png", 0, 0, this.computeX(80, 80), "auto");
         str += "</div>\n";
+        */
 
-        // cc.params['B_KONF'].v == 3
-        str += "<div " + this.createStyle("position:absolute; z-index: 1;", 520, 5, 370, 570) + "\">";
-        str += this.createText("69 °C", 32, "color: #0000ff;",
-        this.computeX(110, 370), this.computeY(290, 570), null, null);
-        str += this.createText("67 °C", 32, "color: #0000ff;",
-        this.computeX(110, 370), this.computeY(485, 570), null, null);
-        str += this.createImage("akunormalno.png",
-        this.computeX(60, 370), this.computeY(232, 570), this.computeX(165, 370), "auto");
-        str += "</div>\n";
+        if (configuration_value === "4. BUF") {
+            // cc.params['B_KONF'].v == 3
+            str += "<div " + this.createStyle("position:absolute; z-index: 1;", 520, 5, 370, 570) + "\">";
+            str += this.createText(buffer_tank_temparature_up_value + " °C", 32, "color: #0000ff;",
+            this.computeX(100, 370), this.computeY(290, 570), null, null);
+            str += this.createText(buffer_tank_temparature_down_value + " °C", 32, "color: #0000ff;",
+            this.computeX(100, 370), this.computeY(485, 570), null, null);
+            str += this.createImage("akunormalno.png",
+            this.computeX(60, 370), this.computeY(232, 570), this.computeX(165, 370), "auto");
+            str += "</div>\n";
+        }
 
+        /*
         // (cc.params['B_addConf'].v &amp;&amp; (cc.params['B_korNum'].v != 255))
         str += "<div " + this.createStyle("position:absolute; z-index: 2;", 240, 85, 100, 50) + "\">";
         str += this.createImage("csk_touch_indicator.png",
@@ -175,31 +222,48 @@ export default class PelTecDisplay {
         str += this.createText("-", 24, "color: #ffffff; text-align: right; z-index: 3;",
         this.computeX(43, 100), this.computeY(13, 50));
         str += "</div>\n";
+        */
 
-        // cc.permissions === '2' &amp;&amp; cc.params['B_STATE'].v &amp;&amp; (cc.params['B_STATE'].v!=='OFF')
-        str += this.createImage("start.gif", 901, 440, 118, null);
+        if (peltec_state !== "OFF") {
+            // cc.permissions === '2' &amp;&amp; cc.params['B_STATE'].v &amp;&amp; (cc.params['B_STATE'].v!=='OFF')
+            str += this.createImage("start.gif", 901, 440, 118, null);
+        }
 
-        // cc.params['B_CMD'].v &amp;&amp; cc.params['B_CMD'].v == 1 &amp;&amp; cc.permissions === '2' &amp;&amp; cc.params['B_STATE'].v &amp;&amp; (cc.params['B_STATE'].v == 'S7-3')
-        str += this.createImage("pauza.png", 942, 390, 40, null);
+        if (active_command == 1 && peltec_state == "S7-3") {
+            // cc.params['B_CMD'].v &amp;&amp; cc.params['B_CMD'].v == 1 &amp;&amp; cc.permissions === '2' &amp;&amp;
+            // cc.params['B_STATE'].v & amp;& amp; (cc.params['B_STATE'].v == 'S7-3')
+            str += this.createImage("pauza.png", 942, 390, 40, null);
+        }
 
-        // cc.params['B_CMD'].v == 1 &amp;&amp; cc.permissions === '2' &amp;&amp; cc.params['B_STATE'].v !== 'OFF' &amp;&amp; cc.params['B_STATE'].v !== 'S7-3'
-        str += this.createImage("playradi.gif", 942, 390, 40, null);
+        if (active_command == 1 && peltec_state !== "OFF" && peltec_state !== "S7-3") {
+            // cc.params['B_CMD'].v == 1 &amp;&amp; cc.permissions === '2' &amp;&amp; cc.params['B_STATE'].v !== 'OFF'
+            // & amp;& amp; cc.params['B_STATE'].v !== 'S7-3'
+            str += this.createImage("playradi.gif", 942, 390, 40, null);
+        }
 
-        str += this.createText("S7-2", 32, "color: #ffffff; text-align: center; z-index: 3;", 900, 360, 120);
+        str += this.createText(peltec_state, 32, "color: #ffffff; text-align: center; z-index: 3;", 900, 360, 120);
 
-        // cc.isLive &amp;&amp; cc.permissions === '2' &amp;&amp; cc.params['B_STATE'].v===undefined || (cc.params['B_STATE'].v==='OFF')
-        // TURN ON BUTTON
+        if (peltec_state == "OFF") {
+            // cc.isLive &amp;&amp; cc.permissions === '2' &amp;&amp; cc.params['B_STATE'].v===undefined || (cc.params['B_STATE'].v==='OFF')
+            // TURN ON BUTTON
+            // TIHOTODO
+        }
 
         // cc.params['B_STATE'].v &amp;&amp; (cc.params['B_STATE'].v === 'OFF')
-        str += this.createText("", 32,
+        str += this.createText("XXX", 32,
         "display:block; background-repeat: no-repeat; background-image: url('/local/lovelace-peltec-card/images/start_stop.png'); background-position: 0px 0px;",
         945, 390, 36, 36);
 
-        //cc.params['B_CMD'].v == 0 &amp;&amp; cc.params['B_STATE'].v !== 'OFF'
-        str += this.createImage("stopradi.gif", 942, 390, 36, "auto");
+        if (active_command == 0 && peltec_state !== "OFF") {
+            //cc.params['B_CMD'].v == 0 &amp;&amp; cc.params['B_STATE'].v !== 'OFF'
+            str += this.createImage("stopradi.gif", 942, 390, 36, "auto");
+        }
 
-        // cc.isLive &amp;&amp; cc.permissions === '2' &amp;&amp; cc.params['B_STATE'].v &amp;&amp; (cc.params['B_STATE'].v!=='OFF')
-        // TURN OFF BUTTON
+        if (peltec_state !== "OFF") {
+            // cc.isLive &amp;&amp; cc.permissions === '2' &amp;&amp; cc.params['B_STATE'].v &amp;&amp; (cc.params['B_STATE'].v!=='OFF')
+            // TURN OFF BUTTON
+            // TIHOTODO
+        }
 
         str += "</div>\n";
         return str;
